e3c71948f260a7e655bdf7a202a5cd9d597bb44a
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-10-13 19:02:12 -0300

    Refatoração das funções utilitárias de acordo com a refatoração das models

--- coruja/utils.py
+++ coruja/utils.py
@@ -1,27 +1,32 @@
+from flask import flash, redirect, url_for
 from sqlalchemy.orm import aliased
 
 from .extensions.database import db
-from .models import Orgao, orgao_administrators
+from .models import Organ, organ_administrators
 
 
-def get_orgaos_by_user_id(user_id) -> None:
-    orgao_admin_alias = aliased(orgao_administrators)
-    orgaos = (
-        Orgao.query.join(
-            orgao_admin_alias, Orgao.id == orgao_admin_alias.c.orgao_id
+def get_organs_by_user_id(user_id: int):
+    organ_admin_alias = aliased(organ_administrators)
+
+    organs = (
+        Organ.query.join(
+            organ_admin_alias,
+            Organ.id == organ_admin_alias.c.orgao_id,
         )
-        .filter(orgao_admin_alias.c.user_id == user_id)
+        .filter(organ_admin_alias.c.user_id == user_id)
         .all()
     )
-    return orgaos
+
+    return organs
 
 
-def insert_orgao(**kwargs) -> None:
+def insert_organ(**kwargs) -> None:
     administrators = kwargs.pop("administrators", [])
-    orgao = Orgao(**kwargs)
+    orgao = Organ(**kwargs)
     db.session.add(orgao)
     db.session.commit()
 
     orgao.administrators.extend(administrators)
+
     db.session.commit()
     db.session.commit()
