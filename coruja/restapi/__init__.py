897cb14de3267acdd7baf07507833977d32ebd6b
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-10-16 17:36:33 -0300

    Implementando uma Blueprint para orgão

--- coruja/restapi/__init__.py
+++ coruja/restapi/__init__.py
@@ -3,9 +3,11 @@ from flask import Flask
 from .api import init_api
 from .application import init_api as init_aplication
 from .auth import init_api as init_auth
+from .organ import init_api as init_organ
 
 
 def init_apis(app: Flask) -> None:
     init_api(app)
     init_auth(app)
     init_aplication(app)
+    init_organ(app)
