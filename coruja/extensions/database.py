4daf5e29ca2f316f179873ff151d19d5d343052a
Felipe A <oie.eu.sou.um@gmail.com>
2023-10-21 16:35:56 -0300

    Adiciona `flask-migrate`

--- coruja/extensions/database.py
+++ coruja/extensions/database.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 
 def init_app(app: Flask) -> None:
     db.init_app(app)
+    migrate.init_app(app, db)
