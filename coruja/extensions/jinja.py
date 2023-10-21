1d0b84f9694f684a75317f36e996b4e812306ecc
Felipe A <oie.eu.sou.um@gmail.com>
2023-10-21 13:45:46 -0300

    Adiciona verificação de obrigatóriedade de uma field

---
+++ coruja/extensions/jinja.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from wtforms import Field
+
+from wtforms.validators import DataRequired
+
+
+
+def is_field_required(field: Field):
+    """Verifica se determinada field possui validador DataRequired
+
+    Args:
+        field (Field): Field
+
+    Returns:
+        bool: Se o field possui validador DataRequired
+    """
+    return any(
+        isinstance(v, DataRequired) for v in field.validators
+    )
+
+def init_app(app: Flask):
+    app.jinja_env.globals.update(
+        is_field_required=is_field_required
+    )
