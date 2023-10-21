f93498462e3525ee49e11a492a113b755b1258d6
thiagooruby <trs3@aluno.ifal.edu.br>
2023-10-21 12:21:44 -0300

    Correção de bugs por conflitos

--- coruja/restapi/institution.py
+++ coruja/restapi/institution.py
@@ -12,7 +12,7 @@ bp = Blueprint("institution", __name__, url_prefix="/instituicao")
 
 @bp.route("/criar", methods=["GET", "POST"])
 @login_required
-def create_institution():
+def get_post_institution_creation():
     """
     Rota para criar uma nova instituição.
 
