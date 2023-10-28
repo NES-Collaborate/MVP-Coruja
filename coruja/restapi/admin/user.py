e6124f3cbbb31330ea6b2cf451714dd1ff4e2789
ecaf1 <edeilsonazevedo22@gamil.com>
2023-10-28 00:05:06 -0300

    Adiciona gestão de usuario

--- coruja/restapi/admin/user.py
+++ coruja/restapi/admin/user.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, request
-
+from ...utils import database_manager
 from ...models import User
-
+from flask_login import login_required
 bp = Blueprint("user", __name__, url_prefix="/user")
 
 
 @bp.route("/", methods=["GET"])
+@login_required
 def show_users():
     page = request.args.get("page", 1, type=int)
     users = User.query.paginate(
@@ -14,3 +15,20 @@ def show_users():
     return render_template(
         "admin/users.html", users=users.items, pagination=users
     )
+
+
+@bp.route("/<int:user_id>/info", methods=["GET"])
+@login_required
+def user_info(user_id: int):
+    """Rota para exibir informações de um usuário específico pelo seu ID.
+
+    Args:
+        user_id (int): ID do usuário cujas informações serão exibidas.
+    """
+    user = database_manager.get_user(user_id)  # Supondo que você tem uma função que pega o usuário pelo ID
+    
+    if not user:
+        flash("Usuário não encontrado", "error")
+        return redirect(url_for("user_list"))  # Supondo que você tem uma rota para listar usuários
+
+    return render_template("admin/user_info.html", user=user)
